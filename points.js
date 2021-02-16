afa968950d6d95a70fe14f78578af81c660d29ff
RSPrasanth <75479170+RSPrasanth@users.noreply.github.com>
2021-02-16 21:10:24 +0530

    Add files via upload

---
+++ points.js
@@ -0,0 +1,4 @@
+text("1,first i will add the background of the application")
+text("2, i will add the buttons")
+text("3,i will add the state names")
+text("4,i will add the state Mechanic association numbers")
